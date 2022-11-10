ccbcdfa535262f2558f7f14d2c8f41ead70a13fa
Tomáš Dámek <tomasdamek123@gmail.com>
2022-11-10 16:25:49 +0100

    RabbitMq fix

--- src/main/java/kiedam/chatapp/backend/event/RabbitMqListener.java
+++ src/main/java/kiedam/chatapp/backend/event/RabbitMqListener.java
@@ -10,11 +10,11 @@ import kiedam.chatapp.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.jms.MessageListener;
 import java.io.IOException;
 
 @Service
